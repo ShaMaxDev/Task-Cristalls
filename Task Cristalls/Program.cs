54af147c500b458f3712c00daeda5a398283d74c
ShaMaxDev <maksimbychkov@yahoo.com>
2023-09-11 21:47:47 -0700

    Add project files.

---
+++ Task Cristalls/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Cristalls
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cristallPrice = 10;
+            int userGold;
+            Console.WriteLine("Hello, adventurer! Today we have cristalls for " + cristallPrice + " gold. How much gold do you have?");
+            userGold = Convert.ToInt32(Console.ReadLine());
+            int cirstallSold = userGold / cristallPrice;
+            int change = userGold % cristallPrice;
+            Console.WriteLine("So, advanturer, now you have " + cirstallSold + " cristalls, and your change is " + change + " gold");
+        }
+    }
+}
