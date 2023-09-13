78ad02b6d05ad88a9c071254b1862b86fe84bd7e
ShaMaxDev <maksimbychkov@yahoo.com>
2023-09-12 18:56:03 -0700

    2 try

--- Task Cristalls/Program.cs
+++ Task Cristalls/Program.cs
@@ -12,11 +12,14 @@ namespace Task_Cristalls
         {
             int cristallPrice = 10;
             int userGold;
+            int cristallToBuy;
             Console.WriteLine("Hello, adventurer! Today we have cristalls for " + cristallPrice + " gold. How much gold do you have?");
             userGold = Convert.ToInt32(Console.ReadLine());
-            int cirstallSold = userGold / cristallPrice;
-            int change = userGold % cristallPrice;
-            Console.WriteLine("So, advanturer, now you have " + cirstallSold + " cristalls, and your change is " + change + " gold");
+            Console.WriteLine("Excellent, adventurer! And how many cristalls do you want to buy?");
+            cristallToBuy = Convert.ToInt32(Console.ReadLine());
+
+            int shortChange = userGold - cristallToBuy * cristallPrice;
+            Console.WriteLine("So, advanturer, here is your " + cristallToBuy + " cristalls, and your change is " + shortChange + " gold");
         }
     }
 }
